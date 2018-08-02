42c711cfd6904f8fdf55dbe28376f84dab92ff8c
arpadt <a.arpad.toth@gmail.com>
2018-08-03 07:06:06 +1000

    Install inquirer. Create first prompt.

--- index.js
+++ index.js
@@ -1,14 +1,28 @@
 const chalk = require('chalk');
 const figlet = require('figlet');
+const inquirer = require('inquirer');
 const vorpal = require('vorpal')();
 
 figlet('Movies', (_, data) => {
   console.log(chalk.cyan(data));
 
   vorpal
-    .command('search', 'Displays the search options')
+    .command('search', 'Start searching')
     .action(async () => {
-      vorpal.log(chalk.yellow('Say hello!'));
+      const movieTitle = await inquirer.prompt([
+        {
+          type: 'input',
+          name: 'movieTitle',
+          message: 'Enter the title or part of the title',
+          validate: (input) => {
+            if (input) return true;
+
+            return 'Please enter title!';
+          }
+        }
+      ]);
+      vorpal.log(chalk.green(JSON.stringify(movieTitle, null, 2)));
+      return;
     });
 
   vorpal
