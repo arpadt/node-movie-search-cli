e3d5ba95fd14e7fcad0f019806827941391504fb
arpadt <a.arpad.toth@gmail.com>
2018-08-03 05:51:53 +1000

    Add basic vorpal setup

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 const chalk = require('chalk');
 const figlet = require('figlet');
-const vorpal = require('vorpal');
-
-const vorpalInstance = vorpal();
+const vorpal = require('vorpal')();
 
 figlet('Movies', (_, data) => {
   console.log(chalk.cyan(data));
+
+  vorpal
+    .command('search', 'Displays the search options')
+    .action(async () => {
+      vorpal.log(chalk.yellow('Say hello!'));
+    });
+
+  vorpal
+    .delimiter('Movies$')
+    .show();
 });
